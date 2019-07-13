4d62fea0d7fa79de8e9207b11e80cd5d4bc8f018
Unknown <dwko@outlook.com>
2019-07-13 13:55:32 +0100

    started new styles

--- nanowire.py
+++ nanowire.py
@@ -22,7 +22,40 @@ tau_y = ta.array(np.kron(s_y, s_0))
 sig_z = ta.array(np.kron(s_0, s_z))
 tau_zsig_x = ta.array(np.kron(s_z, s_x))
 
-def make_system(W=7, L=20, barrierPos=(0, 2, 18, 20),
+def make2DSystem(W=10, L=100, barrierPos=(0, 2, 98, 100),
+                mu=1., B=5, Delta=.2, alpha=0.4, t=1.0):
+    # On each site, electron and hole orbitals.
+    lat = kwant.lattice.square(norbs=4) 
+    
+    # Initialise empty tight-binding model
+    syst = kwant.Builder()
+    
+    # Super
+    syst[(lat(x, y) for x in range(L) for y in range(W))] = \
+        (4 * t - mu) * tau_z + B * sig_z + Delta * tau_x
+        
+    barrier = .2 + mu
+    # Two tunnel barriers
+    syst[(lat(x, y) for x in range(barrierPos[0], barrierPos[1])
+         for y in range(W))] = (4 * t + barrier) * tau_z  
+    syst[(lat(x, y) for x in range(barrierPos[2], barrierPos[3])
+         for y in range(W))] = (4 * t + barrier) * tau_z
+
+    # Hoppings
+    syst[lat.neighbors()] = -t * tau_z - .5j * alpha * tau_zsig_x
+    
+    # Normal metal lead
+    sym_left = kwant.TranslationalSymmetry((-1, 0))
+    lead = kwant.Builder(sym_left, conservation_law=-tau_z, particle_hole=tau_y)
+    lead[(lat(0, j) for j in range(W))] = (4 * t - mu) * tau_z
+    lead[lat.neighbors()] = -t * tau_z
+
+    syst.attach_lead(lead)
+    syst.attach_lead(lead.reversed())
+
+    return syst
+
+def make3DSystem(W=7, L=20, barrierPos=(0, 2, 18, 20),
                 mu=1., B=5, Delta=.2, alpha=0.4, t=1.0):
     # On each site, electron and hole orbitals.
     lat = kwant.lattice.cubic(norbs=4)
@@ -68,7 +101,7 @@ def make_system(W=7, L=20, barrierPos=(0, 2, 18, 20),
 
     return syst
 
-def plotConductanceLead(syst, energies):
+def plotConductanceSystem(syst, energies):
     # Compute conductance at the first lead
     data = []
     for energy in energies:
@@ -77,47 +110,53 @@ def plotConductanceLead(syst, energies):
         data.append(smatrix.submatrix((0, 0), (0, 0)).shape[0] # N
             - smatrix.transmission((0, 0), (0, 0))      # R_ee l1 to l1
             - smatrix.transmission((1, 0), (0, 0))      # R_ee l2 to l1
-            + smatrix.transmission((0, 1), (0, 0)))     # R_he
+            + smatrix.transmission((1, 0), (1, 1))
+            + smatrix.transmission((0, 1), (0, 0))
+            )     # R_he
     plt.figure()
     plt.plot(energies, data)
     plt.xlabel("energy [t]")
     plt.ylabel("conductance [e^2/h]")
     plt.show()
 
-def plotConductanceSystem(syst, energies):
+def plotConductanceLead(syst, energies):
     data = []
     for energy in energies:
         smatrix = kwant.smatrix(syst, energy)
-        # Conductance is N - R_ee + R_he
-        data.append(smatrix.submatrix((0, 0), (0, 0)).shape[0]
-            + smatrix.transmission((0, 0), (1, 0))
-            + smatrix.transmission((1, 0), (1, 0))
-            + smatrix.transmission((1, 0), (1, 1)))# e to h
+        data.append(smatrix.transmission(1, 0))# from lead0 to lead1
     plt.figure()
     plt.plot(energies, data)
     plt.xlabel("energy [t]")
     plt.ylabel("conductance [e^2/h]")
     plt.show()
 
-def family_color(site):
+def family3Dcolor(site):
     x, y, z = site.pos
     if 0 <= y < 3 or 17 <=y < 20:
         return 'green'
     else:
         return 'blue'
+    
+def family2Dcolor(site):
+    x, y = site.pos
+    if 0 <= x < 2 or 98 <= x < 100:
+        return 'green'
+    else:
+        return 'blue'
 
 def main():
-    syst = make_system()
-    plt.rcParams["figure.figsize"] = (12,8)
-    kwant.plot(syst, site_color=family_color)
+    syst = make2DSystem(W=8, barrierPos=(0, 2, 98, 100),
+        mu=.4, B=0., Delta=.2, alpha=0.4, t=1.0)
+    plt.rcParams["figure.figsize"] = (8,5)
+    kwant.plot(syst, site_color=family2Dcolor)
 #    kwant.plot(syst, site_size=0.18, site_lw=0.01, hop_lw=0.1)
 
     # Finalize the system.
     syst = syst.finalized()
 
     # Compute and plot the conductance
-    plotConductanceLead(syst, energies=[0.005 * i for i in range(-120, 120)])
-#    plotConductanceSystem(syst, energies=[0.05 * i for i in range(-50, 50)])
+    plotConductanceSystem(syst, energies=[0.005 * i for i in range(-80, 80)])
+    plotConductanceLead(syst, energies=[0.05 * i for i in range(-50, 50)])
 
 if __name__ == '__main__':
     main()
