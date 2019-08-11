Fixed conductance formula!!

nwDataGenerator.py
@@ -14,10 +14,10 @@ os.system("clear")
 # L = 20:       0 -> 2
 # L = 100:      0 -> 10
 # L = 500:      0 -> 50
-W = 4
-L = 70
+W = 5
+L = 100
 minPeriod = 0
-maxPeriod = .5
+maxPeriod = 2.
 periodBs = np.arange(minPeriod,maxPeriod+.5,.5)
 print("\nGenerating Nanowire Data (periodB = %2.1f --> %2.1f)..." 
       %(minPeriod,maxPeriod))
@@ -28,13 +28,13 @@ for i in range(np.size(periodBs)):
     
     ## Spectrum ##
     pickle.dump(nanowire.spectrum(bValues=np.linspace(0, 1, 51)),
-                open("zspec" 
+                open("tspec" 
                      + "%i.%i.%2.1f" %(W, L, periodBs[i])
                      + ".dat", "wb"))
     
     ## Conductances ##
     pickle.dump(nanowire.conductances(bValues=np.linspace(0, 1, 51)),
-                open("zcond"
+                open("tcond"
                      + "%i.%i.%2.1f" %(W, L, periodBs[i])
                      + ".dat", "wb"))
 

nwObjects.py
@@ -11,36 +11,37 @@ import tinyarray as ta
 import numpy as np
 import scipy.sparse.linalg
 
-s_0 = np.identity(2)
-s_z = np.array([[1., 0.], [0., -1.]])
-s_x = np.array([[0., 1.], [1., 0.]])
-s_y = np.array([[0., -1j], [1j, 0.]])
+s0 = np.identity(2)
+sZ = np.array([[1., 0.], [0., -1.]])
+sX = np.array([[0., 1.], [1., 0.]])
+sY = np.array([[0., -1j], [1j, 0.]])
 
-tau_z = ta.array(np.kron(s_z, s_0))
-tau_x = ta.array(np.kron(s_x, s_0))
-tau_y = ta.array(np.kron(s_y, s_0))
-sig_z = ta.array(np.kron(s_0, s_z))
-sig_x = ta.array(np.kron(s_0, s_x))
-sig_y = ta.array(np.kron(s_0, s_y))
-tau_zsig_x = ta.array(np.kron(s_z, s_x))
-tau_zsig_y = ta.array(np.kron(s_z, s_y))
+tauZ = ta.array(np.kron(sZ, s0))
+tauX = ta.array(np.kron(sX, s0))
+tauY = ta.array(np.kron(sY, s0))
+sigZ = ta.array(np.kron(s0, sZ))
+sigX = ta.array(np.kron(s0, sX))
+sigY = ta.array(np.kron(s0, sY))
+tauZsigX = ta.array(np.kron(sZ, sX))
+tauZsigY = ta.array(np.kron(sZ, sY))
+tauYsigY = ta.array(np.kron(sY, sY))
 
 ## Functions ##
 def makeNISIN(W=5, L=20, barrierLen=1, periodB=.5, isWhole=True):
     def onsiteSc(site, t, B, Delta):
         if periodB == 0:
-            return (4 * t) * tau_z + B * sig_z + Delta * tau_x
+            return (4 * t) * tauZ + B * sigX + Delta * tauX
         else:
             theta = 2*np.pi*periodB*(site.pos[0] - barrierLen)/(L - 1 - 2*barrierLen)
-            return (4 * t) * tau_z + B * (sig_z*np.cos(theta) + sig_x*np.sin(theta)) + Delta * tau_x
+            return (4 * t) * tauZ + B * (sigX*np.cos(theta) - sigY*np.sin(theta)) + Delta * tauX
     def onsiteNormal(site, mu, t):
-        return (4 * t - mu) * tau_z
+        return (4 * t - mu) * tauZ
     def onsiteBarrier(site, mu, t, barrier):
-        return (4 * t - mu + barrier) * tau_z
+        return (4 * t - mu + barrier) * tauZ
     def hopX(site0, site1, t, alpha):
-        return -t * tau_z + .5j * alpha * tau_zsig_y
+        return -t * tauZ + .5j * alpha * tauZsigY
     def hopY(site0, site1, t, alpha):
-        return -t * tau_z - .5j * alpha * tau_zsig_x    
+        return -t * tauZ - .5j * alpha * tauZsigX
     
     # On each site, electron and hole orbitals.
     lat = kwant.lattice.square(norbs=4) 
@@ -60,8 +61,8 @@ def makeNISIN(W=5, L=20, barrierLen=1, periodB=.5, isWhole=True):
     
         # N's
         lead = kwant.Builder(kwant.TranslationalSymmetry((-1,0)),
-                             conservation_law=-tau_z,
-                             particle_hole=tau_y
+                             conservation_law=-tauZ,
+                             particle_hole=tauYsigY
                              )
         lead[(lat(0, j) for j in range(W))] = onsiteNormal
         lead[kwant.builder.HoppingKind((1, 0), lat, lat)] = hopX
@@ -90,7 +91,7 @@ class Nanowire:
                          isWhole=False)
         energies = []
         params = dict(
-                mu=.3, Delta=.1, alpha=.8, t=1.0, barrier = 2.
+                mu=.3, Delta=.1, alpha=.8, t=1.0, barrier=2.
                 )
         for i in tqdm(range(np.size(bValues)), 
                       desc= "Spec for periodB = %2.1f" %(self.periodB)
@@ -106,7 +107,7 @@ class Nanowire:
     
     def conductances(self, 
                      bValues=np.linspace(0, 0.35, 36),
-                     energies=[0.0001 * i for i in range(-1200, 1200)]
+                     energies=[0.001 * i for i in range(-130, 130)]
                      ):
         syst = makeNISIN(W=self.width, L=self.length, 
                          barrierLen=self.barrierLen, 
@@ -114,7 +115,7 @@ class Nanowire:
                          isWhole=True)
         data = []
         params = dict(
-                mu=.3, Delta=.1, alpha=.8, t=1.0, barrier = 2.
+                mu=.3, Delta=.1, alpha=.8, t=1.0, barrier=2.
                 )
         for i in tqdm(range(np.size(energies)), 
                       desc= "Cond for periodB = %2.1f" %(self.periodB)
@@ -124,7 +125,10 @@ class Nanowire:
             for b in bValues:
                 params["B"] = b
                 smatrix = kwant.smatrix(syst, energy, params=params)
-                cond.append(smatrix.transmission(1, 0))
+                cond.append(
+                        smatrix.submatrix((0, 0), (0, 0)).shape[0]  # N
+                        - smatrix.transmission((0, 0), (0, 0))      # R_ee
+                        + smatrix.transmission((0, 1), (0, 0)))     # R_he
             data.append(cond)
             
         outcome = dict(B=bValues, BiasV=energies, Cond=data)