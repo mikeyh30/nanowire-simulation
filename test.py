aa3845aa7ecb7ded42f049c5a4648be453f463cb
michael <ucapmhy@ucl.ac.uk>
2019-10-16 11:27:43 +0100

    Fix nanomagnet_field for numpy inputs

--- test.py
+++ test.py
@@ -4,19 +4,14 @@ import matplotlib.pyplot as plt
 def nanomagnet_field(theta,ratio):
     norm_theta = theta%(2*np.pi)
     norm_ratio = (ratio*2*np.pi)
-    if (norm_theta < norm_ratio):
-        new_theta = norm_theta/(2*ratio)
-    elif (norm_theta >= norm_ratio):
-        new_theta = np.pi+(norm_theta-norm_ratio)/(2-2*ratio)
-    else:
-        raise ValueError('input to nanomagnet_field is wrong.')
+
+    new_theta = np.where(norm_theta < norm_ratio,  norm_theta/(2*ratio),
+                         np.pi+(norm_theta-norm_ratio)/(2-2*ratio)
+                        )
+
     return np.sin(new_theta)
 
 x = np.linspace(0,6*np.pi,1000)
-y = []
 
-for i in x:
-    y.append(nanomagnet_field(i,0.2))
-
-plt.plot(x,y)
+plt.plot(x,nanomagnet_field(x,0.2))
 plt.show()
