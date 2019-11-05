3422e4b6ff47d3e3dc54a94d4fdb52d7bd3d412b
michael <ucapmhy@ucl.ac.uk>
2019-11-05 16:23:01 +0000

    Generate curve generated in Rick's simulations.

--- nanomagnet_field.py
+++ nanomagnet_field.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
 def staggered_sinusoid(theta,ratio):
     norm_theta = theta%(2*np.pi)
@@ -21,7 +22,15 @@ def staggered_cosinusoid(theta,ratio):
     new_theta = new_theta + np.pi/2
     return np.sin(new_theta)
 
+def rick_sinusiod(theta):
+    df = pd.read_csv('./2pi_1D_slice.csv')
+    norm_theta = (theta/(2*np.pi))%1
+    Xrange = df.shape[0]
+    new_theta = np.floor(Xrange*norm_theta)
+    return (df['v'].loc[new_theta], df['u'].loc[new_theta])
+
 if __name__ == "__main__":
     x = np.linspace(0,6*np.pi,1000)
-    plt.plot(x,staggered_sinusoid(x,0.5))
+    # plt.plot(x,staggered_sinusoid(x,0.5))
+    plt.plot(x,rick_sinusiod(x)[0])
     plt.show()
