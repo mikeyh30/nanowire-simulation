7b19b8f68563f22e9c796caccc182b2e8222dafb
michael <ucapmhy@ucl.ac.uk>
2019-12-03 16:24:49 +0000

    Easier to update date for the directory structure

--- gen_data_csv.py
+++ gen_data_csv.py
@@ -2,8 +2,10 @@ import pandas as pd
 from simulation_parameters import simulation_parameters
 from itertools import product
 
-d = [dict(zip(simulation_parameters, v)) for v in product(*simulation_parameters.values())]
+def gen_data_csv(date):
+    d = [dict(zip(simulation_parameters, v)) for v in product(*simulation_parameters.values())]
+    df = pd.DataFrame(d)
+    df.to_csv('./'+date+'.csv',sep=',',index=False)
 
-df = pd.DataFrame(d)
-
-df.to_csv('./2019-12-02.csv',sep=',',index=False)
+if __name__ == "__main__":
+    gen_data_csv("2019-12-02")
