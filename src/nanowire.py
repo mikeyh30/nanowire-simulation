a1bcebfa1b69b231add5b16483a77ae21a784f28
michael <ucapmhy@ucl.ac.uk>
2020-01-09 18:19:26 +0000

    Possible improvements to model More work needed though

--- src/nanowire.py
+++ src/nanowire.py
@@ -12,131 +12,10 @@ import numpy as np
 import scipy.sparse.linalg
 from nanomagnet_field import rick_fourier
 from scipy.constants import physical_constants, hbar
+from transport_model import NISIN
 
 bohr_magneton = 1  # physical_constants['Bohr magneton'][0]
 lattice_constant_InAs = 1  # 6.0583E-10 # might need to change this.
-
-s0 = np.identity(2)
-sZ = np.array([[1.0, 0.0], [0.0, -1.0]])
-sX = np.array([[0.0, 1.0], [1.0, 0.0]])
-sY = np.array([[0.0, -1j], [1j, 0.0]])
-
-tauZ = ta.array(np.kron(sZ, s0))
-tauX = ta.array(np.kron(sX, s0))
-tauY = ta.array(np.kron(sY, s0))
-sigZ = ta.array(np.kron(s0, sZ))
-sigX = ta.array(np.kron(s0, sX))
-sigY = ta.array(np.kron(s0, sY))
-tauZsigX = ta.array(np.kron(sZ, sX))
-tauZsigY = ta.array(np.kron(sZ, sY))
-tauYsigY = ta.array(np.kron(sY, sY))
-
-## Functions ##
-def makeNISIN(
-    width=7,
-    noMagnets=5,
-    barrierLen=1,
-    M=0.05,
-    addedSinu=False,
-    isWhole=True,
-    stagger_ratio=0.5,
-):
-    length = 8 * noMagnets - 2 + 2 * barrierLen  # "2*(noMagnets - 1)"
-
-    ## Define site Hopping and functions ##
-    def hopX(site0, site1, t, alpha):
-        return -t * tauZ + 1j * alpha * tauZsigY
-
-    def hopY(site0, site1, t, alpha):
-        return -t * tauZ - 1j * alpha * tauZsigX
-
-    def sinuB(theta, stagger_ratio):
-        # ssin, scos = rick_fourier(theta)
-        # return sigY*scos + sigX*ssin
-        return sigY * np.cos(theta) + sigX * np.sin(theta)
-
-    # This is the onsite Hamiltonian, this is where the B-field can be varied.
-    def onsiteSc(site, muSc, t, B, Delta):
-        gfactor = 2  # should be 10 in the real units
-        if addedSinu:
-            counter = np.mod(site.pos[0] - 1 - barrierLen, 16)
-            if -1 < counter < 4:
-                theta = 0
-            elif 3 < counter < 8:
-                theta = 0.2 * (counter - 3) * np.pi
-            elif 7 < counter < 12:
-                theta = np.pi
-            else:
-                theta = 0.2 * (counter - 6) * np.pi
-
-            return (
-                (4 * t - muSc) * tauZ
-                + 0.5 * gfactor * bohr_magneton * B * sigX
-                + Delta * tauX
-                + 0.5 * gfactor * bohr_magneton * M * sinuB(theta, stagger_ratio)
-            )
-        else:
-            return (
-                (4 * t - muSc) * tauZ
-                + 0.5 * gfactor * bohr_magneton * B * sigX
-                + Delta * tauX
-            )
-
-    def onsiteNormal(site, mu, t):
-        return (4 * t - mu) * tauZ
-
-    def onsiteBarrier(site, mu, t, barrier):
-        return (4 * t - mu + barrier) * tauZ
-
-    # On each site, electron and hole orbitals.
-    lat = kwant.lattice.square(norbs=4)
-    syst = kwant.Builder()
-
-    # S
-    syst[
-        (
-            lat(i, j)
-            for i in range(barrierLen, length - barrierLen)
-            for j in range(width)
-        )
-    ] = onsiteSc
-
-    if isWhole:
-        # I's
-        syst[
-            (lat(i, j) for i in range(barrierLen) for j in range(width))
-        ] = onsiteBarrier
-        syst[
-            (
-                lat(i, j)
-                for i in range(length - barrierLen, length)
-                for j in range(width)
-            )
-        ] = onsiteBarrier
-
-        # Hopping:
-        syst[kwant.builder.HoppingKind((1, 0), lat, lat)] = hopX
-        syst[kwant.builder.HoppingKind((0, 1), lat, lat)] = hopY
-
-        # N's
-        lead = kwant.Builder(
-            kwant.TranslationalSymmetry((-1, 0)),
-            conservation_law=-tauZ,
-            particle_hole=tauYsigY,
-        )
-        lead[(lat(0, j) for j in range(width))] = onsiteNormal
-        lead[kwant.builder.HoppingKind((1, 0), lat, lat)] = hopX
-        lead[kwant.builder.HoppingKind((0, 1), lat, lat)] = hopY
-
-        syst.attach_lead(lead)
-        syst.attach_lead(lead.reversed())
-    else:
-        # Hopping:
-        syst[kwant.builder.HoppingKind((1, 0), lat, lat)] = hopX
-        syst[kwant.builder.HoppingKind((0, 1), lat, lat)] = hopY
-
-    return syst.finalized()
-
 
 ## Objects ##
 class Nanowire:
@@ -145,7 +24,7 @@ class Nanowire:
         width=5,
         noMagnets=5,
         dim=2,
-        barrierLen=1,
+        barrier_length=1,
         effective_mass=0.5,
         M=0.05,
         muSc=0.0,
@@ -160,7 +39,7 @@ class Nanowire:
         self.width = width
         self.noMagnets = noMagnets
         self.dim = dim
-        self.barrierLen = barrierLen
+        self.barrier_length = barrier_length
 
         # Superconducting components
         self.t = 0.5 / effective_mass # (hbar**2)/(2*effective_mass*lattice_constant_InAs)
@@ -178,22 +57,18 @@ class Nanowire:
         self.barrier = barrier
 
         # System
-        """self.system = makeNISIN(width=self.width, noMagnets=self.noMagnets, 
-                                barrierLen=self.barrierLen, M=self.M,
-                                addedSinu=self.addedSinu, isWhole=False,
+        """self.system = NISIN(width=self.width, noMagnets=self.noMagnets, 
+                                barrier_length=self.barrier_length, M=self.M,
+                                addedSinu=self.addedSinu, 
                                 stagger_ratio=self.stagger_ratio
                                 )
         """
 
     def spectrum(self, bValues=np.linspace(0, 1.0, 201)):
-        syst = makeNISIN(
+        syst = NISIN(
             width=self.width,
             noMagnets=self.noMagnets,
-            barrierLen=self.barrierLen,
-            M=self.M,
-            addedSinu=self.addedSinu,
-            isWhole=False,
-            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length,
         )
         energies = []
         critB = 0
@@ -204,6 +79,10 @@ class Nanowire:
             alpha=self.alpha,
             t=self.t,
             barrier=self.barrier,
+            addedSinu=self.addedSinu,
+            M=self.M,
+            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length
         )
         for i in tqdm(
             range(np.size(bValues)),
@@ -228,14 +107,10 @@ class Nanowire:
         bValues=np.linspace(0, 1.0, 201),
         energies=[0.001 * i for i in range(-120, 120)],
     ):
-        syst = makeNISIN(
+        syst = NISIN(
             width=self.width,
             noMagnets=self.noMagnets,
-            barrierLen=self.barrierLen,
-            M=self.M,
-            addedSinu=self.addedSinu,
-            isWhole=True,
-            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length
         )
         data = []
         critB = 0
@@ -246,6 +121,10 @@ class Nanowire:
             alpha=self.alpha,
             t=self.t,
             barrier=self.barrier,
+            M=self.M,
+            addedSinu=self.addedSinu,
+            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length
         )
         for i in tqdm(
             range(np.size(energies)),
@@ -276,14 +155,10 @@ class Nanowire:
     def criticals(
         self, bValues=np.linspace(0, 0.4, 81), muValues=np.linspace(0, 1.0, 101)
     ):
-        syst = makeNISIN(
+        syst = NISIN(
             width=self.width,
             noMagnets=self.noMagnets,
-            barrierLen=self.barrierLen,
-            M=self.M,
-            addedSinu=self.addedSinu,
-            isWhole=False,
-            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length,
         )
         criticalPoints = []
         params = dict(
@@ -292,6 +167,10 @@ class Nanowire:
             alpha=self.alpha,
             t=self.t,
             barrier=self.barrier,
+            M=self.M,
+            addedSinu=self.addedSinu,
+            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length
         )
         for i in tqdm(
             range(np.size(muValues)),
@@ -316,14 +195,10 @@ class Nanowire:
     def phaseTransitions(
         self, bValues=np.linspace(0, 0.4, 81), muValues=np.linspace(0, 1.0, 101)
     ):
-        syst = makeNISIN(
+        syst = NISIN(
             width=self.width,
             noMagnets=self.noMagnets,
-            barrierLen=self.barrierLen,
-            M=self.M,
-            addedSinu=self.addedSinu,
-            isWhole=False,
-            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length,
         )
         phases = []
         params = dict(
@@ -332,6 +207,10 @@ class Nanowire:
             alpha=self.alpha,
             t=self.t,
             barrier=self.barrier,
+            M=self.M,
+            addedSinu=self.addedSinu,
+            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length
         )
         for i in tqdm(
             range(np.size(bValues)),
@@ -358,14 +237,10 @@ class Nanowire:
     def phaseAid(
         self, bValues=np.linspace(0.0, 0.3, 61), muValues=np.linspace(0.2, 0.5, 61)
     ):
-        syst = makeNISIN(
+        syst = NISIN(
             width=self.width,
             noMagnets=self.noMagnets,
-            barrierLen=self.barrierLen,
-            M=self.M,
-            addedSinu=self.addedSinu,
-            isWhole=False,
-            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length,
         )
         energies0 = []
         energies1 = []
@@ -376,6 +251,10 @@ class Nanowire:
             alpha=self.alpha,
             t=self.t,
             barrier=self.barrier,
+            M=self.M,
+            addedSinu=self.addedSinu,
+            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length
         )
         for i in tqdm(
             range(np.size(bValues)), desc="Number of magnets = %i" % (self.noMagnets)
@@ -401,14 +280,10 @@ class Nanowire:
         return outcome
 
     def plot(self):
-        syst = makeNISIN(
+        syst = NISIN(
             width=self.width,
             noMagnets=self.noMagnets,
-            barrierLen=self.barrierLen,
-            M=self.M,
-            addedSinu=self.addedSinu,
-            isWhole=False,
-            stagger_ratio=self.stagger_ratio,
+            barrier_length=self.barrier_length,
         )
 
         return kwant.plotter.plot(syst, show=False)
