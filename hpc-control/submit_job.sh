f737c94f05655e0ca8eebd4dd8ef112e97078edc
Michael Hynes <ucapmhy@login13.myriad.ucl.ac.uk>
2020-03-26 18:00:16 +0000

    Slight correctiong to hpc submission script

--- hpc-control/submit_job.sh
+++ hpc-control/submit_job.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-ARRAY_SIZE=$(nwsetup test | tr -d -c 0-9)
-qsub -t 1-$ARRAY_SIZE array_script.sh $1
+ARRAY_SIZE=$(nwsetup $1 | tr -d -c 0-9)
+qsub -t 1-$ARRAY_SIZE /home/ucapmhy/nanowire-simulations/nanowire-simulation/hpc-control/array_script.sh $1
