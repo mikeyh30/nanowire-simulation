f8a32a335864948ae7f6c9f11f00640257d47641
Michael Hynes <mikey.hynes@hotmail.co.uk>
2020-04-22 23:00:59 +0100

    Include configuration setup in setup.py

--- README.md
+++ README.md
@@ -9,11 +9,6 @@ This repository contains code for performing simulations of an InAs nanowire usi
  git clone https://github.com/mikeyh30/nanowire-simulation.git
  cd nanowire-simulation
  pip install .
-
- # Setup configuration files
- cp config/globals-sample.yml globals.yml
- cp config/sim_parameters_units-sample.yml sim_parameters_units.yml
- cp config/sim_parameters_unitless-sample.yml sim_parameters_unitless.yml
 ```
 
-Change the paths in ```globals.yml```  to point to the scratch, src, and hpc-control folder.
+Change the path to the scratch folder in ```globals.yml``` to point to the scratch folder.
--- setup.py
+++ setup.py
@@ -1,4 +1,58 @@
+import os
+from shutil import copyfile
 from setuptools import setup, find_packages
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+
+
+def run_installer(command_subclass):
+    command_subclass_run = command_subclass.run
+
+    def modified_run(self):
+        # Complete traditional installation
+        command_subclass_run(self)
+        # Define config file locations
+        globals_sample = os.path.join(os.getcwd(), "config", "globals-sample.yml")
+        units_sample = os.path.join(os.getcwd(), "config", "sim_parameters_units-sample.yml")
+        unitless_sample = os.path.join(os.getcwd(), "config", "sim_parameters_unitless-sample.yml")
+        globals_file = os.path.join(os.getcwd(), "globals.yml")
+        units_file = os.path.join(os.getcwd(), "sim_parameters_units.yml")
+        unitless_file = os.path.join(os.getcwd(), "sim_parameters_unitless.yml")
+        # Copy config files across
+        copyfile(globals_sample, globals_file)
+        copyfile(units_sample, units_file)
+        copyfile(unitless_sample, unitless_file)
+
+        # Update globals
+        # Read in the file
+        with open('globals.yml', 'r') as file:
+            filedata = file.read()
+
+        scratch = os.path.join(os.getcwd(), "data/")
+        src = os.path.join(os.getcwd(), "src/")
+        hpc_control = os.path.join(os.getcwd(), "hpc-control/")
+
+        # Replace the target string
+        filedata = filedata.replace("/path/to/scratch/folder/", scratch)
+        filedata = filedata.replace("/path/to/src/folder/", src)
+        filedata = filedata.replace("/path/to/hpc-control/folder/", hpc_control)
+
+        # Write the file out again
+        with open('globals.yml', 'w') as file:
+            file.write(filedata)
+
+    command_subclass.run = modified_run
+    return command_subclass
+
+
+@run_installer
+class SetupConfigurationInstall(install):
+    pass
+
+@run_installer
+class SetupConfigurationDevelop(develop):
+    pass
+
 
 setup(
     name="nanowire-simulation",
@@ -25,5 +79,9 @@ setup(
             "simulate = simulate.simulation:main",
             "hpc_simulate = simulate.hpc_simulation:main",
         ]
+    },
+    cmdclass={
+        "install": SetupConfigurationInstall,
+        "develop": SetupConfigurationDevelop,
     },
 )
